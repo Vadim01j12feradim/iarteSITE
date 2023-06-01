55586092be933119fcd38254881bf325ad545a27
Vadim01j12feradim <izmael.guzman7281@alumnos.udg.mx>
2023-05-31 19:48:56 -0600

    sfasd

--- conexion.php
+++ conexion.php
@@ -1,10 +1,19 @@
 <?php
+    function conectar(){
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "convocatoria";
+        $conexion = new mysqli($servername, $username, $password, $dbname);
+        $conexion->set_charset("utf8");
+        
+        // Comprobar la conexión
+        if ($conexion->connect_error) {
+            die("Conexión fallida: " . $conexion->connect_error);
+        }else{
+            echo "Conexión exitosa";
+            return $conexion;
+        }   
+    }
 
-
-
-?>ody>
-    <php>
-
-</body>
-
-</html>
+?>
