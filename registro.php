78c49144b7f49bb60ef170011a2b96e5502b09da
Vadim01j12feradim <izmael.guzman7281@alumnos.udg.mx>
2023-09-20 19:03:38 -0600

    adsa

--- registro.php
+++ registro.php
@@ -1,3 +1,9 @@
+<!-- REDIRECT HERROR -->
+
+<?php
+    header('Location: index.html');
+?>
+
 <html  class=" js flexbox canvas canvastext webgl no-touch geolocation postmessage no-websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients no-cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths"><head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
